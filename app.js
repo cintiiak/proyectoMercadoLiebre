cc940153173e8479895816a87eadcd5ee38503a6
cintiiak <cintiazamudio35@gmail.com>
2024-01-12 16:17:04 -0300

    Instalacion de express

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require ("express");
+const app = expres();
+
+/**
+ * Metodo listen para levantar el servidor 
+ */
+app.listen (3000,()=>{
+    console.log("Servidor corriendo...")
+})
