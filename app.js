3132ebf3c6be4164d45e47de4e722bd9ecb05ca8
cintiiak <cintiazamudio35@gmail.com>
2024-01-12 16:50:30 -0300

    Configuracion y creacion de ruta en archivo app.js y actualizacion en archivo home.html

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const express = require ("express");
-const app = expres();
+const express = require ('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
 
 /**
  * Metodo listen para levantar el servidor 
  */
 app.listen (3000,()=>{
-    console.log("Servidor corriendo...")
+    console.log(`Servidor corriendo.. Listening on port http://localhost:${port}`)
+})
+
+/**
+ * Path del Home 
+ */
+app.get('/',(req,res) =>{
+    res.sendFile(path.join(__dirname, '/views/home.html'));
 })
