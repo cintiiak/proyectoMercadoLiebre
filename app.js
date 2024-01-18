4750f3e242a186edbbe7c95463a1eee9b76ab3ae
cintiiak <cintiazamudio35@gmail.com>
2024-01-18 20:16:07 -0300

    html para formularios registro y login

--- app.js
+++ app.js
@@ -18,6 +18,20 @@ app.get('/',(req,res) =>{
 })
 
 /**
+ * 
+ */
+app.get('/register',(req,res) =>{
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+})
+
+/**
+ * 
+ */
+app.get('/login',(req,res) =>{
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+})
+
+/**
  * Acceso a carpeta public estatica
  */
 app.use(express.static('public'));
